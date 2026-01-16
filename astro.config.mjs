d1dee74ff6d31c8b1c2dffc5d05620998f15d967
Ajith Kumar A R <>
2026-01-17 00:18:55 +0530

    Server Level Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
